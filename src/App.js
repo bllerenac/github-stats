cd828be8d6464bd31376d8f64f6b8e6c3124ce4f
Codekacode <karem.quiroz.m@gmail.com>
2021-03-04 23:55:05 -0500

    Add type color size to icon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import './components/UI/icons.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Icon  type={home} color="blue" size="20" />
     </div>
   );
 }
--- src/components/UI/icons.js
+++ src/components/UI/icons.js
@@ -1,14 +1,37 @@
-function Icon({type, fill, size, onClick}) {
+/** @jsxImportSource @emotion/react */
+
+import {
+    RiHome2Fill,
+    RiStarFill,
+    RiStarLine,
+    RiUserHeartFill,
+    RiSearchFill,
+} from "react-icons/ri";
+import { FaUsers } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { css } from "@emotion/react";
+
+const setIcon = {
+    home: RiHome2Fill,
+    star: RiStarFill,
+    starLine: RiStarLine,
+    followers: FaUsers,
+    followings: RiUserHeartFill,
+    search: RiSearchFill,
+    github: FaGithub,
+}
+
+function Icon({type, color, size, onClick}) {
     const IconToRender = setIcon[type];
     return ( 
         <IconToRender 
             onClick={onClick}
             css={css`
-            fill: ${fill};
+            color: ${color};
             font-size: ${size}px;
             `}
         />        
     );    
 };
 
-export Icon;
+export default Icon;
