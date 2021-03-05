f83628a28e8c519529fa6bbb7c6540b4e090fa13
Codekacode <karem.quiroz.m@gmail.com>
2021-03-05 12:51:33 -0500

    add home template

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import Button from './components/UI/Button'
 import Icon from './components/UI/Icon'
 import NavComponent from './components/containers/Nav'
 import Avatar from "./components/UI/Avatar";
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       
       <Icon type="home" color="blue" size="20" />
-      <Avatar />
-      <NavComponent />
-      <Button />
+      <Home/>
+      
     
     </div>
   );
