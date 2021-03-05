0ebf77e0d26d543b082b7f8206eca361b64bba1b
Codekacode <karem.quiroz.m@gmail.com>
2021-03-05 08:36:09 -0500

    Add style to button

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,3 +1,19 @@
+import styled from "@emotion/styled";
+
+const StyledButton =  styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 16px;
+    height: 36px;
+    background: #2d9cdb;
+    box-shadow: 2px 2px 0px rgba(0, 0, 0, 0.25);
+    border-radius: 4px;
+    font-weight: bold;
+    font-size: 16px;
+    
+    `;
+
 function Button() {
     return (
 
