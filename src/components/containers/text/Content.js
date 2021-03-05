8fcf0cc536f358a96df2fd1f4df972a1af7b081a
Frank Dominguez <elvisf.dominguez@gmail.com>
2021-03-05 10:30:36 -0500

    Create ContentPrimaryBlod component in Content.js

--- src/components/containers/text/Content.js
+++ src/components/containers/text/Content.js
@@ -19,9 +19,18 @@ const StyledContentPrimary = styled(Basic)`
   font-size: 20px;
   line-height: 25px;
 `;
+const StyledContentPrimaryBold = styled(Basic)`
+  font-size: 20px;
+  line-height: 25px;
+  font-weight: 700;
+`;
 
 function ContentPrimary({ children, color }) {
   return <StyledContentPrimary color={color} children={children} />;
 }
 
-export { ContentPrimary };
+function ContentPrimaryBold({ children, color }) {
+  return <StyledContentPrimaryBold color={color} children={children} />;
+}
+
+export { ContentPrimary, ContentPrimaryBold };
