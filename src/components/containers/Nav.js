41de40f043e80f6e3695fa63fcc9f8b23b736508
Frank Dominguez <elvisf.dominguez@gmail.com>
2021-03-05 19:11:14 -0500

    Add keys to icons in the navbar component it will remove the alerts

--- src/components/containers/Nav.js
+++ src/components/containers/Nav.js
@@ -1,41 +1,39 @@
 /** @jsxImportSource @emotion/react */
 import styled from "@emotion/styled";
-import Icon from '../UI/Icon'
+import Icon from "../UI/Icon";
 
 const StyledNav = styled.div`
-    height: 66px;
-    padding: 8px 0;
+  height: 66px;
+  padding: 8px 0;
+  display: flex;
+  justify-content: center;
+  box-shadow: 0px -2px 0px rgba(0, 0, 0, 0.25);
+  .icons-wrap {
+    width: 250px;
+    height: 50px;
     display: flex;
-    justify-content: center;
-    box-shadow: 0px -2px 0px rgba(0, 0, 0, 0.25);
-    .icons-wrap {
-        width: 250px;
-        height: 50px;
-        display: flex;
-        justify-content: space-between;
-    }
-`
+    justify-content: space-between;
+  }
+`;
 
-
-function NavComponent({css}){
-    const icon_nav = ["home", "search", "star"];
-    return (
-        <StyledNav css={css}>
-            <div className="icons-wrap">
-               {icon_nav.map(name_icon =>{
-                 
-                        return(
-                            <Icon
-                            type={name_icon}
-                            color={"#828282"}
-                            size={50}
-                            />
-                        )
-               } 
-               )}
-            </div>
-        </StyledNav>
-    )
+function NavComponent({ css }) {
+  const icon_nav = ["home", "search", "star"];
+  return (
+    <StyledNav css={css}>
+      <div className="icons-wrap">
+        {icon_nav.map((name_icon) => {
+          return (
+            <Icon
+              key={name_icon}
+              type={name_icon}
+              color={"#828282"}
+              size={50}
+            />
+          );
+        })}
+      </div>
+    </StyledNav>
+  );
 }
 
 export default NavComponent;
